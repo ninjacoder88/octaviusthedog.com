360a11a0f9773e2c05bad2c268ec82ac0c352427
Tom Harris <tomharris88@outlook.com>
2020-10-25 15:16:49 -0500

    Initialized depdencies

---
+++ src/OctaviusTheDog/wwwroot/js/app/main.js
@@ -0,0 +1,14 @@
+﻿define(function (require) {
+    var $ = require("jquery");
+    var ko = require("knockout");
+
+    function ViewModel() {
+        var self = this;
+        self.pictures = ko.observableArray([]);
+        self.go = function () {
+            alert("went");
+        };
+    };
+
+    ko.applyBindings(new ViewModel(), document.getElementById("container"));
+});
---
+++ src/OctaviusTheDog/wwwroot/js/requireConfig.js
@@ -0,0 +1,21 @@
+﻿requirejs.config({
+    baseUrl: "js/lib",
+    paths: {
+        app: "../app",
+        knockout: "https://cdnjs.cloudflare.com/ajax/libs/knockout/3.5.0/knockout-min",
+        jquery: "https://code.jquery.com/jquery-3.5.1.min",
+        bootstrap: "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+    },
+    onNodeCreated: function (node, config, module, path) {
+        var sri = {
+            bootstrap: "sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+        };
+
+        if (sri[module]) {
+            node.setAttribute("integrity", sri[module]);
+            node.setAttribute("crossorigin", "anonymous")
+        }
+    }
+});
+
+requirejs(["app/main"]);
